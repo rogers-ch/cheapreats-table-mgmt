76e79d96cb2f17bd352a08f365354544197eb4a8
ScottyKnows44 <Scottcfalcon@gmail.com>
2020-12-04 13:05:46 -0800

    Converstion to styled components

--- gatsby-site/src/components/ButtonComponent.tsx
+++ gatsby-site/src/components/ButtonComponent.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import styled from 'styled-components';
+
 //interface for the ButtonComponent, use this to create the button
 interface IButtonComponent{
     userType: string,
@@ -10,7 +13,22 @@ export const ButtonComponent: React.FC <IButtonComponent> = ({
 }: IButtonComponent) => {
     return (
         <div>
-            <button className="btn btn-danger btn-lg border-light rounded" >{userType}</button>
+            <ButtonStyling>{userType}</ButtonStyling>
         </div>
     );
 };
+
+const ButtonStyling = styled.div`
+    border-color: #f8f9fa;
+    border-radius: 0.25rem;
+    padding: 0.5rem 1rem;
+    line-height: 1.5;
+    color: #fff;
+    background-color: #dc3545;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    border: 1px solid transparent;
+    display: inline-block;
+    user-select: none;
+    text-align: center;
+    vertical-align: middle;
+`;
--- gatsby-site/src/components/MainContainer.js
+++ gatsby-site/src/components/MainContainer.js
@@ -8,22 +8,44 @@
 import React from 'react';
 import './../css/App.css';
 import CustomerInfo from "./CustomerInfo";
+import styled from 'styled-components';
 
 function MainContainer() {
 
   return (
 
-    <div className="pt-6">
-        <h1 className="pt-3">Table Management Coming Soon!</h1><hr></hr>
+    <Pt6>
+        <Pt3WithH3>Table Management Coming Soon!</h1><hr></hr>
 
-        <p className="pt-2 pb-3 styleFont">Comprehensive restaurant management made easy.</p>
-        <p className="pb-3 styleFont">Seat customers, make reservations, track orders,and
-          get a birds-eye view of your dining areas from one simple interface!</p>
-        <p className="pb-3 styleFont">To see a demo  of a restaurant design, click to the left.</p>
+        <Pt2Pb3 className="styleFont">Comprehensive restaurant management made easy.</Pt2Pb3>
+        <Pb3 className="styleFont">Seat customers, make reservations, track orders,and
+          get a birds-eye view of your dining areas from one simple interface!</Pb3>
+        <Pb3 className="styleFont">To see a demo  of a restaurant design, click to the left.</Pb3>
 
         <CustomerInfo />
     </div>
   );
 }
+
+const Pb3 = styled.div`
+    padding-bottom: 1rem;
+`;
+
+const Pt2Pb3 = styled.div`
+    padding-bottom: 1rem;
+    padding-top: 0.5rem;
+`;
+
+const Pt3WithH3 = styled.div`
+    padding-top: 1rem;
+    font-size: 2.5rem;
+    font-family: 'Lato', sans-serif;
+`;
+
+const Pt6 = styled.div`
+    padding-top: 3.5rem;
+    padding-bottom: 1rem;
+    background-color: rgb(245, 245, 245);
+`;
 
 export default MainContainer;
