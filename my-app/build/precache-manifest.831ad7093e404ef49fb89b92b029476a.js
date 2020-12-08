350a527da78e11d3be5358282c56a2f1aaaf69c9
rogers-ch <crogers25@mail.greenriver.edu>
2020-12-08 10:39:26 -0800

    Add updated build folder

---
+++ my-app/build/precache-manifest.831ad7093e404ef49fb89b92b029476a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66066563cf4871aa33bf9898e6af027c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc7ffb21c6461f4771e",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "73ad0bfc771dbdb87a98",
+    "url": "/static/css/main.ac98bd1c.chunk.css"
+  },
+  {
+    "revision": "9dc7ffb21c6461f4771e",
+    "url": "/static/js/2.ff641bc2.chunk.js"
+  },
+  {
+    "revision": "825b3f1924aa91c0b22224e02cce27f9",
+    "url": "/static/js/2.ff641bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ad0bfc771dbdb87a98",
+    "url": "/static/js/main.191ac154.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
