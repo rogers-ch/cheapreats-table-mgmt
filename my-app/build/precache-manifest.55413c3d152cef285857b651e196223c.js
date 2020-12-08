82f15f09321068b0f8c1ea85189679e2e917a730
rogers-ch <crogers25@mail.greenriver.edu>
2020-12-07 17:21:35 -0800

    Update build folder with demo on frontpage - new build folder

---
+++ my-app/build/precache-manifest.55413c3d152cef285857b651e196223c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5bfa736c425890ec2053190c221e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123a99bcd8a3f9220145",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "6d2b15a77d91ac75d7d4",
+    "url": "/static/css/main.64200fff.chunk.css"
+  },
+  {
+    "revision": "123a99bcd8a3f9220145",
+    "url": "/static/js/2.6ea7c8d6.chunk.js"
+  },
+  {
+    "revision": "825b3f1924aa91c0b22224e02cce27f9",
+    "url": "/static/js/2.6ea7c8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2b15a77d91ac75d7d4",
+    "url": "/static/js/main.16306b27.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "57a0e1c487a76ce6ca45edc50b4a8cc8",
+    "url": "/static/media/DiningRoomLightBackground.57a0e1c4.png"
+  },
+  {
+    "revision": "814aa99e5e51a37a06f5151b8441a1fd",
+    "url": "/static/media/DinningRoomImage.814aa99e.png"
+  },
+  {
+    "revision": "e5a893d4a1cfab588b03e4ebe4932a11",
+    "url": "/static/media/SquareTableImage.e5a893d4.png"
+  }
+]);
